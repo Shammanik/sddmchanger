d8ace807b372084c75a73cc844c9ba7fe1ce55f2
Shamanikkk <tmatveyv@gmail.com>
2026-07-29 20:58:31 +0300

    Add output of themes

--- sddmchanger.sh
+++ sddmchanger.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+num=0
+arr=()
+
+themes=$(ls /usr/share/sddm/themes/)
+
+echo "Enter selected theme:"
+for i in $(echo "$themes"); do
+	echo "$num $i"
+	arr[num]="$i"
+	((num++))
+done
