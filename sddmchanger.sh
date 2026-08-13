bf0ba06969a76d0d6a89c2a6817d91062bb4a5d0
Shamanikkk <tmatveyv@gmail.com>
2026-08-13 17:28:33 +0300

    Add info about your theme

--- sddmchanger.sh
+++ sddmchanger.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-#source test.conf
-source /etc/sddm.conf
+
+sddm=/etc/sddm.conf
 num=0
 arr=()
 
+current=$(grep -Po '(?<==).*' "$sddm")
 themes=$(ls /usr/share/sddm/themes/)
+
+echo -e "\nYour current theme is: \e[31m$current\e[0m\n"
 
 echo "Enter selected theme:"
 for i in $(echo "$themes"); do
@@ -12,7 +15,7 @@ for i in $(echo "$themes"); do
 	arr[num]="$i"
 	((num++))
 done
+
 read select
-#sed -i "s/^Current=.*/Current=${arr[select]}/" test.conf
-sudo sed -i "s/^Current=.*/Current=${arr[select]}/" /etc/sddm.conf
+sudo sed -i "s/^Current=.*/Current=${arr[select]}/" "$sddm"
 echo "Theme changed on ${arr[select]}"
